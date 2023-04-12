1e40c84e5ff9f8d5ee6284792389aa3940649129
richardmachage <rchacha261@gmail.com#>
2023-04-12 16:39:03 +0300

    scalar to Moshi converter

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,24 +17,32 @@
 
 package com.example.android.marsrealestate.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
 
 private const val BASE_URL = "https://mars.udacity.com/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 //use retrofit builder
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 //implement the interface
 interface MarsApiService {
     @GET("realestate")
-    fun getProperties(): Call<String>
+    fun getProperties():
+            Call<List<MarsProperty>>
 }
 
 //create the MarsApi object using retrofit to implement the marsapiaservice
